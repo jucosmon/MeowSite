68b0218c011eebe0f2e41dcc1f1026dbda65ba0e
jucosmon <udtohanrj@gmail.com>
2023-05-09 17:16:34 +0800

    added random cat facts frontend and backend, but saving functionality not done yet

--- index.php
+++ index.php
@@ -4,6 +4,14 @@ include("connection.php");
 include("functions.php");
 
 $user_data = check_login($con);
+
+// Fetch random cat GIF from the Cat API
+$endpoint = "https://api.thecatapi.com/v1/images/search?mime_types=gif";
+$catGifData = file_get_contents($endpoint);
+$catGifData = json_decode($catGifData, true);
+
+// Extract the GIF URL
+$gifUrl = $catGifData[0]['url'];
 ?>
 
 <!DOCTYPE html>
@@ -16,7 +24,7 @@ $user_data = check_login($con);
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Galindo&display=swap" rel="stylesheet" />
-  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" type="text/css" href="style.css?v=1" />
   <title>MeowSite</title>
 </head>
 
@@ -43,6 +51,35 @@ $user_data = check_login($con);
         <?php echo $user_data['nickname']; ?>!
       </h1>
       <img src="img/cat (11).png" alt="Cat cute pic">
+    </div>
+  </div>
+
+  <div class="random-facts">
+    <div>
+      <img src="img/anal-gland-expression.png" alt="Funny cat back">
+      <h2>Kitty Trivia</h2>
+    </div>
+    <p>
+      <?php
+      // Fetch random cat fact from the API
+      $apiEndpoint = "https://meowfacts.herokuapp.com/";
+      $catFactData = file_get_contents($apiEndpoint);
+
+      // Decode the JSON response
+      $catFact = json_decode($catFactData, true);
+
+      // Extract the cat fact text
+      $factText = $catFact['data'][0];
+
+      // Display the cat fact
+      echo $factText;
+      ?>
+    </p>
+    <img src="<?php echo $gifUrl; ?>" alt="Random Cat gif" id="cat-gif">
+
+    <div class="buttons">
+      <a href="#"><button id="paw"> <img src="img/pets.png" alt="Paw" id="paw-img"> </button></a>
+      <a href="index.php"><button id="first_button">Next</button></a>
     </div>
   </div>
 
